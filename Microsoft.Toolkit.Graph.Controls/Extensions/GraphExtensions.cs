9b5d8d979b7c15531cb1350d056c5b7c81f3ea61
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2020-06-23 16:59:07 -0700

    Initial test of OS Person Card usage
    Couldn't get Thumbnail to appear

---
+++ Microsoft.Toolkit.Graph.Controls/Extensions/GraphExtensions.cs
@@ -0,0 +1,80 @@
+﻿
+using System.Diagnostics;
+using Windows.ApplicationModel.Contacts;
+using Windows.System.UserProfile;
+
+namespace Microsoft.Toolkit.Graph.Controls.Extensions
+{
+    public static class GraphExtensions
+    {
+        public static Contact ToWindowsContact(this Microsoft.Graph.Person person)
+        {
+            var contact = new Contact()
+            {
+                RemoteId = person.Id,
+                DisplayNameOverride = person.DisplayName,
+                // Note: Trying to set individual name portions causes UnhandledException in contract call
+            };
+
+            foreach (var phoneInfo in person.Phones)
+            {
+                contact.Phones.Add(new ContactPhone()
+                {
+                    Kind = phoneInfo.Type.ToPhoneKind(),
+                    Number = phoneInfo.Number,
+                });
+            }
+
+            foreach (var emailInfo in person.EmailAddresses)
+            {
+                contact.Emails.Add(new ContactEmail()
+                {
+                    Address = emailInfo.Address,
+                });
+            }
+
+            contact.JobInfo.Add(new ContactJobInfo()
+            {
+                Title = person.Title ?? string.Empty,
+                CompanyName = person.CompanyName ?? string.Empty,
+                CompanyYomiName = person.YomiCompany ?? string.Empty,
+                Department = person.Department ?? string.Empty,
+                Office = person.OfficeLocation ?? string.Empty,
+            });
+
+            return contact;
+        }
+
+        public static ContactPhoneKind ToPhoneKind(this Microsoft.Graph.PhoneType? phoneType)
+        {
+            if (phoneType is null)
+            {
+                return ContactPhoneKind.Other;
+            }
+
+            switch (phoneType.Value)
+            {
+                case Microsoft.Graph.PhoneType.Home:
+                    return ContactPhoneKind.Home;
+                case Microsoft.Graph.PhoneType.Business:
+                    return ContactPhoneKind.Company;
+                case Microsoft.Graph.PhoneType.Mobile:
+                    return ContactPhoneKind.Mobile;
+                default:
+                case Microsoft.Graph.PhoneType.Other:
+                case Microsoft.Graph.PhoneType.OtherFax:
+                    return ContactPhoneKind.Other;
+                case Microsoft.Graph.PhoneType.Assistant:
+                    return ContactPhoneKind.Assistant;
+                case Microsoft.Graph.PhoneType.HomeFax:
+                    return ContactPhoneKind.HomeFax;
+                case Microsoft.Graph.PhoneType.BusinessFax:
+                    return ContactPhoneKind.BusinessFax;
+                case Microsoft.Graph.PhoneType.Pager:
+                    return ContactPhoneKind.Pager;
+                case Microsoft.Graph.PhoneType.Radio:
+                    return ContactPhoneKind.Radio;
+            }
+        }
+    }
+}
--- SampleTest/MainPage.xaml.cs
+++ SampleTest/MainPage.xaml.cs
@@ -2,7 +2,18 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.Toolkit.Graph.Controls;
+using Microsoft.Toolkit.Graph.Controls.Extensions;
+using Microsoft.Toolkit.Graph.Extensions;
+using Microsoft.Toolkit.Graph.Providers;
+using System.IO;
+using Windows.ApplicationModel.Contacts;
+using Windows.Foundation;
+using Windows.Storage.Streams;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace SampleTest
 {
@@ -14,6 +25,35 @@ namespace SampleTest
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private IRandomAccessStream _stream;
+
+        // Testing https://docs.microsoft.com/en-us/windows/uwp/design/controls-and-patterns/contact-card
+        private async void PersonView_PointerPressed(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            if (ContactManager.IsShowContactCardSupported() && sender is PersonView pv)
+            {
+                Rect selectionRect = GetElementRect((FrameworkElement)sender);
+
+                var contact = pv.PersonDetails.ToWindowsContact();
+
+                // Test hack (how to re-use image we have already?)
+                _stream = (await ProviderManager.Instance.GlobalProvider.Graph.GetUserPhoto(pv.PersonDetails.UserPrincipalName)).AsRandomAccessStream();
+
+                contact.Thumbnail = RandomAccessStreamReference.CreateFromStream(_stream);
+
+                ContactManager.ShowContactCard(contact, selectionRect, Placement.Below);
+            }
+        }
+
+        // Gets the rectangle of the element 
+        public static Rect GetElementRect(FrameworkElement element)
+        {
+            // Passing "null" means set to root element. 
+            GeneralTransform elementTransform = element.TransformToVisual(null);
+            Rect rect = elementTransform.TransformBounds(new Rect(0, 0, element.ActualWidth, element.ActualHeight));
+            return rect;
         }
     }
 }
