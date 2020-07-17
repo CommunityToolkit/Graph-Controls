601218e551818b22fd289d23e0a273506cd7a18d
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2020-07-17 16:00:34 -0700

    Add WinForms XAML Islands Example

---
+++ Samples/XAML Islands/WinForms-Core-GraphApp/Form1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Toolkit.Graph.Providers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms_Core_GraphApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            ProviderManager.Instance.GlobalProvider = new MockProvider();
+
+            InitializeComponent();
+
+            windowsXamlHost1.InitialTypeName = "UWP_XamlApplication.SamplePage";
+        }
+    }
+}
