32c10b84d4df173cb3673e07398e91ce98341ce4
Shane Weaver <shweaver@microsoft.com>
2021-03-24 12:49:41 -0700

    Update SampleTest/App.xaml.cs
    Co-authored-by: Nikola Metulev <nmetulev@users.noreply.github.com>

--- SampleTest/App.xaml.cs
+++ SampleTest/App.xaml.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using CommunityToolkit.Net.Authentication;
-using CommunityToolkit.Net.Authentication.Msal;
 using System;
 using System.Threading.Tasks;
 using Windows.ApplicationModel;
