468cdcd861f960685a856ae90dde0b6eaf0349c4
Shane Weaver <shweaver@microsoft.com>
2021-04-28 13:41:02 -0700

    Updates to baseRoamingSettingsDataStore  (#99)
    * Updates to baseRoamingSettingsDataStore so serializization/deserialization is more consistent
    
    * Added comments
    
    * Updated serializer in OneDriveDataSource

--- CommunityToolkit.Uwp.Graph.Controls/Helpers/RoamingSettings/OneDriveDataStore.cs
+++ CommunityToolkit.Uwp.Graph.Controls/Helpers/RoamingSettings/OneDriveDataStore.cs
@@ -22,10 +22,11 @@ namespace CommunityToolkit.Uwp.Graph.Helpers.RoamingSettings
         /// <typeparam name="T">The type of object to retrieve.</typeparam>
         /// <param name="userId">The id of the target Graph user.</param>
         /// <param name="fileName">The name of the file.</param>
+        /// <param name="serializer">An object serializer for handling deserialization.</param>
         /// <returns>The deserialized file contents.</returns>
-        public static async Task<T> Get<T>(string userId, string fileName)
+        public static async Task<T> Get<T>(string userId, string fileName, IObjectSerializer serializer)
         {
-            return await OneDriveDataSource.Retrieve<T>(userId, fileName);
+            return await OneDriveDataSource.Retrieve<T>(userId, fileName, serializer);
         }
 
         /// <summary>
@@ -35,10 +36,11 @@ namespace CommunityToolkit.Uwp.Graph.Helpers.RoamingSettings
         /// <param name="userId">The id of the target Graph user.</param>
         /// <param name="fileName">The name of the file.</param>
         /// <param name="fileContents">The object to store.</param>
+        /// <param name="serializer">An object serializer for handling serialization.</param>
         /// <returns>A task.</returns>
-        public static async Task Set<T>(string userId, string fileName, T fileContents)
+        public static async Task Set<T>(string userId, string fileName, T fileContents, IObjectSerializer serializer)
         {
-            await OneDriveDataSource.Update<T>(userId, fileName, fileContents);
+            await OneDriveDataSource.Update(userId, fileName, fileContents, serializer);
         }
 
         /// <summary>
@@ -81,20 +83,20 @@ namespace CommunityToolkit.Uwp.Graph.Helpers.RoamingSettings
         /// <inheritdoc />
         public override async Task<bool> FileExistsAsync(string filePath)
         {
-            var roamingSettings = await Get<object>(UserId, Id);
+            var roamingSettings = await Get<object>(UserId, Id, Serializer);
             return roamingSettings != null;
         }
 
         /// <inheritdoc />
         public override async Task<T> ReadFileAsync<T>(string filePath, T @default = default)
         {
-            return await Get<T>(UserId, filePath) ?? @default;
+            return await Get<T>(UserId, filePath, Serializer) ?? @default;
         }
 
         /// <inheritdoc />
         public override async Task<StorageFile> SaveFileAsync<T>(string filePath, T value)
         {
-            await Set(UserId, filePath, value);
+            await Set(UserId, filePath, value, Serializer);
 
             // Can't convert DriveItem to StorageFile, so we return null instead.
             return null;
@@ -110,7 +112,7 @@ namespace CommunityToolkit.Uwp.Graph.Helpers.RoamingSettings
                 IDictionary<string, object> remoteData = null;
                 try
                 {
-                    remoteData = await Get<IDictionary<string, object>>(UserId, fileName);
+                    remoteData = await Get<IDictionary<string, object>>(UserId, fileName, Serializer);
                 }
                 catch
                 {
@@ -145,7 +147,7 @@ namespace CommunityToolkit.Uwp.Graph.Helpers.RoamingSettings
                 if (needsUpdate)
                 {
                     // Send updates for local values, overwriting the remote.
-                    await Set(UserId, fileName, Cache);
+                    await Set(UserId, fileName, Cache, Serializer);
                 }
 
                 SyncCompleted?.Invoke(this, new EventArgs());
