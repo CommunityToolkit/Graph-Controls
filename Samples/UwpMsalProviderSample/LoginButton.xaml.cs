a7c53db47f798eb70a3bbc4c8657da99a5d3f1d7
Shane Weaver <shweaver@microsoft.com>
2021-05-20 16:33:35 -0700

    Removed preview controls/helpers and any references (#103)
    * Removed preview controls/helpers and any references

---
+++ Samples/UwpMsalProviderSample/LoginButton.xaml.cs
@@ -0,0 +1,70 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.Authentication;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UwpMsalProviderSample
+{
+    /// <summary>
+    /// A simple button for triggering the globally configured IProvider to sign in and out.
+    /// </summary>
+    public partial class LoginButton : UserControl
+    {
+        public LoginButton()
+        {
+            InitializeComponent();
+
+            ProviderManager.Instance.ProviderUpdated += (s, e) => UpdateState();
+            UpdateState();
+        }
+
+        private void UpdateState()
+        {
+            var provider = ProviderManager.Instance.GlobalProvider;
+            if (provider == null || provider.State == ProviderState.Loading)
+            {
+                MyButton.Content = "Sign in";
+                IsEnabled = false;
+                return;
+            }
+
+            switch (provider.State)
+            {
+                case ProviderState.SignedIn:
+                    MyButton.Content = "Sign out";
+                    break;
+
+                case ProviderState.SignedOut:
+                    MyButton.Content = "Sign in";
+                    break;
+            }
+
+            IsEnabled = true;
+        }
+
+        private async void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                var provider = ProviderManager.Instance.GlobalProvider;
+                if (provider != null)
+                {
+                    switch (provider.State)
+                    {
+                        case ProviderState.SignedOut:
+                            provider.SignInAsync();
+                            break;
+
+                        case ProviderState.SignedIn:
+                            provider.SignOutAsync();
+                            break;
+                    }
+                }
+            });
+        }
+    }
+}
