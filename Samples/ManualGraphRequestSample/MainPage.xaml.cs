d8bded48669ca80c77122759347fbfee8ee9b43a
Shane Weaver <shweaver@microsoft.com>
2021-05-26 16:54:09 -0700

    Added sample demonstrating manual graph call (#105)
    * Added manual Graph call sample

---
+++ Samples/ManualGraphRequestSample/MainPage.xaml.cs
@@ -0,0 +1,107 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.Authentication;
+using ManualGraphRequestSample.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ManualGraphRequestSample
+{
+    public sealed partial class MainPage : Page
+    {
+        public ObservableCollection<TodoTask> TaskCollection = new ObservableCollection<TodoTask>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ProviderManager.Instance.ProviderUpdated += OnProviderUpdated;
+            ProviderManager.Instance.GlobalProvider = new WindowsProvider(new string[] { "User.Read", "Tasks.ReadWrite" });
+        }
+
+        private async void OnProviderUpdated(object sender, ProviderUpdatedEventArgs e)
+        {
+            IProvider provider = ProviderManager.Instance.GlobalProvider;
+            
+            switch (provider?.State)
+            {
+                case ProviderState.Loading:
+                    SignInButton.Content = "Loading...";
+                    SignInLoadingRing.IsActive = true;
+                    SignInButton.IsEnabled = false;
+                    break;
+
+                case ProviderState.SignedOut:
+                    SignInButton.Content = "Sign In";
+                    SignInLoadingRing.IsActive = false;
+                    SignInButton.IsEnabled = true;
+
+                    TaskCollection.Clear();
+                    break;
+
+                case ProviderState.SignedIn:
+                    SignInButton.Content = "Sign Out";
+                    SignInLoadingRing.IsActive = false;
+                    SignInButton.IsEnabled = true;
+
+                    IList<TodoTask> tasks = await GetDefaultTaskListAsync();
+                    if (tasks != null)
+                    {
+                        foreach (var task in tasks)
+                        {
+                            TaskCollection.Add(task);
+                        }
+                    }
+                    break;
+            }
+        }
+
+        private async void OnSignInButtonClick(object sender, RoutedEventArgs e)
+        {
+            IProvider provider = ProviderManager.Instance.GlobalProvider;
+            if (provider?.State == ProviderState.SignedOut)
+            {
+                await ProviderManager.Instance.GlobalProvider.SignInAsync();
+            }
+            else if (provider?.State == ProviderState.SignedIn)
+            {
+                await ProviderManager.Instance.GlobalProvider.SignOutAsync();
+            }
+        }
+
+        private async Task<IList<TodoTask>> GetDefaultTaskListAsync()
+        {
+            var httpClient = new HttpClient();
+            var requestUri = "https://graph.microsoft.com/v1.0/me/todo/lists/tasks/tasks";
+
+            var getRequest = new HttpRequestMessage(HttpMethod.Get, requestUri);
+            await ProviderManager.Instance.GlobalProvider.AuthenticateRequestAsync(getRequest);
+
+            using (httpClient)
+            {
+                var response = await httpClient.SendAsync(getRequest);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+                    var jObject = JObject.Parse(jsonResponse);
+                    if (jObject.ContainsKey("value"))
+                    {
+                        var tasks = JsonConvert.DeserializeObject<List<TodoTask>>(jObject["value"].ToString());
+                        return tasks;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Samples/ManualGraphRequestSample/Models/TodoTask.cs
@@ -0,0 +1,43 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Newtonsoft.Json;
+
+namespace ManualGraphRequestSample.Models
+{
+    /// <summary>
+    /// The type Todo Task.
+    /// The complete type definition can refer to here: https://github.com/microsoftgraph/msgraph-sdk-dotnet/blob/dev/src/Microsoft.Graph/Generated/model/TodoTask.cs
+    /// </summary>
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+    public class TodoTask
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TodoTask"/> class.
+        /// </summary>
+        public TodoTask()
+        {
+            this.ODataType = "microsoft.graph.todoTask";
+        }
+
+        /// <summary>
+        /// Gets or sets @odata.type.
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "@odata.type", Required = Required.Default)]
+        public string ODataType { get; set; }
+
+        /// <summary>
+        /// Gets or sets id.
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "id", Required = Required.Default)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets title.
+        /// A brief description of the task.
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "title", Required = Required.Default)]
+        public string Title { get; set; }
+    }
+}
