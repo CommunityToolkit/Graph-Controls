140776e0518e15d80ab96c6fce2d9153f1d84827
Anran Zhang <anranzhang@microsoft.com>
2021-06-04 15:44:20 +0800

    add header

--- Samples/UwpWindowsProviderSample/AccountManagerButton.xaml.cs
+++ Samples/UwpWindowsProviderSample/AccountManagerButton.xaml.cs
@@ -1,23 +1,16 @@
-﻿using CommunityToolkit.Authentication;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.Authentication;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace UwpWindowsProviderSample
 {
+    /// <summary>
+    /// A simple button for triggering the globally configured WindowsProvider to manage account.
+    /// </summary>
     public sealed partial class AccountManagerButton : UserControl
     {
         public AccountManagerButton()
