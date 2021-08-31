3ca626410a5464a32ed256f32094c67912cb5c06
Shane Weaver <shweaver@microsoft.com>
2021-08-31 09:18:17 -0700

    Experimental attribute for GraphPresenter and sample updates (#148)
    * Added experimental attributes to GraphPresenter and QueryOption
    
    * Moved GraphPresenter samples into separate pages and added another for OneDrive
    
    * Converting converters into simple methods

--- SampleTest/Samples/GraphPresenterSample.xaml.cs
+++ SampleTest/Samples/GraphPresenterSample.xaml.cs
@@ -14,58 +14,6 @@ namespace SampleTest.Samples
 {
     public sealed partial class GraphPresenterSample : Page
     {
-        // Workaround for https://github.com/microsoft/microsoft-ui-xaml/issues/2407
-        public DateTime Today => DateTimeOffset.Now.Date.ToUniversalTime();
-
-        // Workaround for https://github.com/microsoft/microsoft-ui-xaml/issues/2407
-        public DateTime ThreeDaysFromNow => Today.AddDays(3);
-
-        public IBaseRequestBuilder CalendarViewBuilder;
-        public IBaseRequestBuilder MessagesBuilder;
-        public IBaseRequestBuilder PlannerTasksBuilder;
-        public IBaseRequestBuilder TeamsChannelMessagesBuilder;
-
-        public GraphPresenterSample()
-        {
-            InitializeComponent();
-
-            ProviderManager.Instance.ProviderUpdated += OnProviderUpdated;
-            ProviderManager.Instance.ProviderStateChanged += OnProviderStateChanged;
-        }
-
-        private void OnProviderStateChanged(object sender, ProviderStateChangedEventArgs e)
-        {
-            if (e.NewState == ProviderState.SignedIn)
-            {
-                var graphClient = ProviderManager.Instance.GlobalProvider.GetClient();
-
-                CalendarViewBuilder = graphClient.Me.CalendarView;
-                MessagesBuilder = graphClient.Me.Messages;
-                PlannerTasksBuilder = graphClient.Me.Planner.Tasks;
-                TeamsChannelMessagesBuilder = graphClient.Teams["02bd9fd6-8f93-4758-87c3-1fb73740a315"].Channels["19:d0bba23c2fc8413991125a43a54cc30e@thread.skype"].Messages;
-            }
-            else
-            {
-                ClearRequestBuilders();
-            }
-        }
-
-        private void OnProviderUpdated(object sender, IProvider provider)
-        {
-            if (provider == null)
-            {
-                ClearRequestBuilders();
-            }
-        }
-
-        private void ClearRequestBuilders()
-        {
-            CalendarViewBuilder = null;
-            MessagesBuilder = null;
-            PlannerTasksBuilder = null;
-            TeamsChannelMessagesBuilder = null;
-        }
-
         public static string ToLocalTime(DateTimeTimeZone value)
         {
             // Workaround for https://github.com/microsoft/microsoft-ui-xaml/issues/2407
@@ -78,15 +26,9 @@ namespace SampleTest.Samples
             return value?.LocalDateTime.ToString("g");
         }
 
-        public static string RemoveWhitespace(string value)
+        public GraphPresenterSample()
         {
-            // Workaround for https://github.com/microsoft/microsoft-ui-xaml/issues/2654
-            return Regex.Replace(value, @"\t|\r|\n", " ");
-        }
-
-        public static bool IsTaskCompleted(int? percentCompleted)
-        {
-            return percentCompleted == 100;
+            InitializeComponent();
         }
     }
 }
