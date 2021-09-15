1ca6164c21b1043bc2c220986064ab4c861f6bfb
Michael Hawker MSFT (XAML Llama) <24302614+michael-hawker@users.noreply.github.com>
2021-09-14 17:45:07 -0700

    Fixes #149 - Update Sample to use External ObservableCollection (#160)
    We re-write the sample to use an external collection as a developer would in their app. This then properly lets the ItemsControl display just the tokenized items. (Though there's still an issue with how TokenizingTextBox behaves we should resolve.)

--- SampleTest/Samples/PeoplePickerSample.xaml.cs
+++ SampleTest/Samples/PeoplePickerSample.xaml.cs
@@ -2,12 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.ObjectModel;
+using Microsoft.Graph;
 using Windows.UI.Xaml.Controls;
 
 namespace SampleTest.Samples
 {
     public sealed partial class PeoplePickerSample : Page
     {
+        ObservableCollection<Person> MyPeople { get; set; } = new();
+
         public PeoplePickerSample()
         {
             InitializeComponent();
